82dec1b7f44e88b0314265f272dbac864255b75f
CaioIan <iancaio015@gmail.com>
2022-03-11 17:42:41 -0300

    Complete Project

---
+++ java-script.js
@@ -0,0 +1,24 @@
+function insert(num) {
+  const numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+}
+
+function clean() {
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function back() {
+  let result = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = result.substring(0, result.length -1 );
+}
+
+function calcular() {
+  let result = document.getElementById('resultado').innerHTML;
+  if(result) {
+    document.getElementById('resultado').innerHTML = eval(result)
+  }
+
+  else{
+    document.getElementById('resultado').innerHTML = "Nada";
+  }
+}
